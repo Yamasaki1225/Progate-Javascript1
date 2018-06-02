8e2b7fd34b0840bbfbef2e388380c2a9bf261c0f
Ryo Yamasaki <yamasaki1600@yahoo.co.jp>
2018-06-02 14:52:10 +0900

    else if

--- hoge.js
+++ hoge.js
@@ -1,6 +1,8 @@
-var level = 10;
-if (level >= 10) {
-  console.log("手裏剣と忍者刀を装備できます");
+var level = 5;
+if (level >= 10){
+  console.log("手裏剣と忍者刀お装備できます");
+} else if (level >= 5 && level < 10) {
+  console.log("忍者刀を装備できます");
 } else {
   console.log("何も装備できません");
 }
