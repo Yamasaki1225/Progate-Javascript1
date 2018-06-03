10898c31e4bb458660308191f23a543620a1e361
Ryo Yamasaki <yamasaki1600@yahoo.co.jp>
2018-06-03 12:20:23 +0900

    while文

--- hoge.js
+++ hoge.js
@@ -1,5 +1,11 @@
-console.log(3);
-console.log(5 + 2);
-console.log(3 - 2);
-console.log(5 + 2);
-console.log("5 + 2");
+var i = 1;
+while(i <= 5) {
+  console.log(i);
+  i += 1
+}
+
+var i = 1;
+while(i <= 100) {
+  console.log(i);
+  i = i + 1
+}
