552f19c6a4e3def68bd567834877fffb6a2f6d85
Ryo Yamasaki <yamasaki1600@yahoo.co.jp>
2018-06-03 16:57:28 +0900

    配列とfor文

--- hoge.js
+++ hoge.js
@@ -1,9 +1,4 @@
-var fruits = ["apple","banana","orange"];
-console.log(fruits);
-console.log(fruits[0]);
-fruits[0] = "grape";
-console.log(fruits[0]);
-console.log(fruits.length);
-fruits.push("apple");
-console.log(fruits);
-
+var fruits = ["apple","banana","orange","grape"];
+for(var i = 0; i < fruits.length; i++) {
+  console.log(fruits[i]);
+}
