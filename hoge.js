0f126d8145b8eb17a53273cc287a3cb46ee3558e
Ryo Yamasaki <yamasaki1600@yahoo.co.jp>
2018-06-03 16:40:04 +0900

    配列

--- hoge.js
+++ hoge.js
@@ -1,3 +1,9 @@
 var fruits = ["apple","banana","orange"];
 console.log(fruits);
 console.log(fruits[0]);
+fruits[0] = "grape";
+console.log(fruits[0]);
+console.log(fruits.length);
+fruits.push("apple");
+console.log(fruits);
+
