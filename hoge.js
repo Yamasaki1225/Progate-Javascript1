ad2816f9c2d8db44cfe8755a1209c0fdba5f700b
Ryo Yamasaki <yamasaki1600@yahoo.co.jp>
2018-06-10 19:21:00 +0900

    返り値

--- hoge.js
+++ hoge.js
@@ -1,5 +1,6 @@
-function introduce(name,age) {
-  console.log("私の名前は" + name + "です")
-  console.log(age + "歳です。");
+function add(a,b) {
+  return a + b;
+  console.log("計算されました") //returnは関数内の処理を終われせる性質がある
 }
-introduce("にんじゃわんこ",14)
+sum = add(1,3);
+console.log(sum)
